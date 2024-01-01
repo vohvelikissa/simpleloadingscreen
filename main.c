8933e5a580a0117604ddc024a7bb72bd5210e422
Pessi <--add>
2024-01-01 17:12:20 +0200

    did a thing

---
+++ main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int args, char * argv[]) {
+	if (args >= 2) {
+		printf("hold on tight you kid.");
+		for (int i = 1; i < args; i++) {
+			printf("processing: %s", argv[i]);
+			system(argv[i]);
+		}
+	} else {
+		printf("Give me something to do you fucking idiot");
+	}
+	return 0;
+}
