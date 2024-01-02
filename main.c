5773a0d78ebc9908c3229c74f16dda71f4a44401
Pessi <--add>
2024-01-02 23:29:53 +0200

    made it good enough

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(int args, char * argv[]) {
 	if (args >= 2) {
 		printf("hold on tight you kid.");
 		for (int i = 1; i < args; i++) {
-			printf("processing: %s", argv[i]);
+			printf("processing: (%d/%d) %s\n", i, args-1, argv[i]);
 			system(argv[i]);
 		}
 	} else {
